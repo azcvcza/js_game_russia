6e7bdad169c08d902d96e9e0690ba1cde9f55695
azcvcza <283940802@qq.com>
2018-11-13 20:58:12 +0800

    feature: 简易跨网页聊天初步

--- wsServer.js
+++ wsServer.js
@@ -1,13 +1,20 @@
 var ws = require("nodejs-websocket");
 var Port = 3000;
+
+var clientCount = 0;
 var server = ws.createServer(function(conn){
     console.log("New connection");
+    clientCount++;
+    conn.nickname ='user'+clientCount;
+    broadcast(conn.nickname+" come in");
     conn.on("text",function(str){
         console.log("received", str);
-        conn.sendText(str.toUpperCase()+"!!!");
+        //conn.sendText(str.toUpperCase()+"!!!");
+        broadcast(conn.nickname+" : "+str);
     });
     conn.on("close",function(code,reason){
         console.log("connection closed");
+        broadcast(conn.nickname+" log out")
     });
     conn.on("error",function(err){
         console.log("handle error",err);
@@ -15,3 +22,9 @@ var server = ws.createServer(function(conn){
 }).listen(Port);
 
 console.log("websocket listen on port:"+Port);
+
+function broadcast(str){
+    server.connections.forEach(function(conn){
+        conn.sendText(str);
+    })
+}
